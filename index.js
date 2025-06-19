3300278ee159c69efc06882c070b7dc061c5a089
Dharm Mehulkumar Bharodiya <39424523+DharmBharodiya@users.noreply.github.com>
2025-06-19 23:12:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 const container = document.querySelector("#gradient-container");
 
+
+let tl = gsap.timeline()
+
+tl
+.to("#fs", {
+    scaleY: 0,
+    duration: 1,
+    delay: 1,
+    ease: Expo.easeInOut,
+    transformOrigin: "top"
+})
+.to("#ss", {
+    height: "100%",
+    duration: 2,
+    delay: -2,
+    ease: Expo.easeInOut    
+})
+.to("#span2", {
+    opacity: 1,
+    delay: -.9
+})
+.to("#ts", {
+    scaleY: 1,
+    duration: 2,
+    delay: -1.1,
+    ease: Expo.easeInOut,
+    transformOrigin: "bottom"
+})
+.to("#gradient-container", {
+    opacity: 1,
+    delay: -.74
+})
+
+
+
 function generateNumber(){
     return Math.floor(Math.random() * 100);
 }
